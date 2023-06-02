905d15012d5ff03c53ba7836757969bdaabf874b
lidj <lidingji1997@hotmail.com>
2023-06-02 15:57:06 +0800

    add license headers

--- src/duvisor/src/mm/utils.rs
+++ src/duvisor/src/mm/utils.rs
@@ -1,3 +1,16 @@
+/*
+Copyright (c) 2023 The institute of parallel and distributed systems (IPADS)
+DuVisor is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+     http://license.coscl.org.cn/MulanPSL2
+
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+*/
+
 pub const PAGE_SIZE_SHIFT: u64 = 12;
 pub const PAGE_TABLE_REGION_SIZE: u64 = 32 << MB_SHIFT; /* 32MB for now */
 pub const PAGE_SIZE: u64 = 1u64 << PAGE_SIZE_SHIFT;
